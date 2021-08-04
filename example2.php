1fab87042a5be96098d9f25c3858d8f264fb4a5c
Waldemar Graban <waldekgraban@gmail.com>
2021-08-04 10:00:10 +0200

    Create example2.php
    If children can be an empty array then you can shorten it to this example

---
+++ example2.php
@@ -0,0 +1,32 @@
+<?php
+$orig = [
+    0 => ['id' => 3230],
+    1 => ['id' => 3308],
+    2 => [
+        'id' => 3310,
+        'children' => [
+            0 => [
+                'id' => 3312,
+            ],
+            1 => [
+                'id' => 3313
+            ]
+        ]
+    ]
+];
+
+$deleted = [3312];
+
+function recurs($array, $deleted) {
+    return array_filter(array_map(function ($elem) use ($deleted) {
+        if (isset($elem['children'])) {
+            $elem['children'] = recurs($elem['children'], $deleted);
+        }
+        
+        return in_array($elem['id'], $deleted) ? null : $elem;
+    }, $array));
+}
+
+$new = recurs($orig, $deleted);
+
+print_r($new);
